3326f075b54587c24dbb9dc2ee296b3d0e523081
Matthew Bonig <matthew.bonig@gmail.com>
2022-02-18 21:00:48 -0700

    updating description and keywords

--- .projenrc.js
+++ .projenrc.js
@@ -3,12 +3,14 @@ const { NpmAccess } = require('projen/lib/javascript');
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Matthew Bonig',
   authorAddress: 'matthew.bonig@gmail.com',
+  description: 'A Step Function state machine construct focused on working well with the Workflow Studio',
   cdkVersion: '2.4.0',
   defaultReleaseBranch: 'main',
   name: '@matthewbonig/state-machine',
   repositoryUrl: 'https://github.com/mbonig/state-machine.git',
   npmAccess: NpmAccess.PUBLIC,
   gitignore: ['.idea/'],
+  keywords: ['awscdk', 'cdk', 'AWS Step Functions'],
 });
 
 project.synth();
