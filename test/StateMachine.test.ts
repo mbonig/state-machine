4492609c35b41a4611b7c5e40e3054c86837f557
Matthew Bonig <matthew.bonig@gmail.com>
2022-04-11 19:26:17 -0600

    uses lodash merge as it does what smash did but handles arrays

--- test/StateMachine.test.ts
+++ test/StateMachine.test.ts
@@ -3,7 +3,7 @@ import { Template } from 'aws-cdk-lib/assertions';
 import { StateMachineType } from 'aws-cdk-lib/aws-stepfunctions';
 import { StateMachine } from '../src';
 
-test('snapshot', () => {
+test('simple snapshot', () => {
   const stack = new Stack(new App(), 'TestingStack', {});
   new StateMachine(stack, 'Test', {
     stateMachineName: 'A-nice-state-machine',
@@ -13,4 +13,251 @@ test('snapshot', () => {
   });
   const assert = Template.fromStack(stack);
   expect(assert.toJSON()).toMatchSnapshot();
+});
+
+test('handles arrays', () => {
+  const stack = new Stack(new App(), 'TestingStack', {});
+  let startingDefinition = {
+    States: {
+      Branches: [
+        {
+          StartAt: 'ResumeCluster',
+          States: {
+            'ResumeCluster': {
+              Type: 'Task',
+              Parameters: {
+                ClusterIdentifier: 'MyData',
+              },
+              Resource: 'arn:aws:states:::aws-sdk:redshift:resumeCluster',
+              Next: 'DescribeClusters',
+            },
+            'DescribeClusters': {
+              Type: 'Task',
+              Parameters: {
+                ClusterIdentifier: '',
+              },
+              Resource: 'arn:aws:states:::aws-sdk:redshift:describeClusters',
+              Next: 'Evaluate Cluster Status',
+            },
+            'Evaluate Cluster Status': {
+              Type: 'Choice',
+              Choices: [
+                {
+                  Variable: '$.Clusters[0].ClusterStatus',
+                  StringEquals: 'available',
+                  Next: 'Redshift Pass',
+                },
+              ],
+              Default: 'Redshift Wait',
+            },
+            'Redshift Pass': {
+              Type: 'Pass',
+              End: true,
+            },
+            'Redshift Wait': {
+              Type: 'Wait',
+              Seconds: 5,
+              Next: 'DescribeClusters',
+            },
+          },
+        },
+        {
+          StartAt: 'StartInstances',
+          States: {
+            'StartInstances': {
+              Type: 'Task',
+              Parameters: {
+                InstanceIds: [
+                  'MyData',
+                ],
+              },
+              Resource: 'arn:aws:states:::aws-sdk:ec2:startInstances',
+              Next: 'DescribeInstanceStatus',
+            },
+            'DescribeInstanceStatus': {
+              Type: 'Task',
+              Next: 'Evaluate Instance Status',
+              Parameters: {
+                InstanceIds: [
+                  'MyData',
+                ],
+              },
+              Resource: 'arn:aws:states:::aws-sdk:ec2:describeInstanceStatus',
+            },
+            'Evaluate Instance Status': {
+              Type: 'Choice',
+              Choices: [
+                {
+                  And: [
+                    {
+                      Variable: '$.InstanceStatuses[0].InstanceState.Name',
+                      StringEquals: 'running',
+                    },
+                    {
+                      Variable: '$.InstanceStatuses[0].SystemStatus.Details[0].Status',
+                      StringEquals: 'passed',
+                    },
+                    {
+                      Variable: '$.InstanceStatuses[0].InstanceStatus.Details[0].Status',
+                      StringEquals: 'passed',
+                    },
+                  ],
+                  Next: 'EC2 Pass',
+                },
+              ],
+              Default: 'EC2 Wait',
+            },
+            'EC2 Pass': {
+              Type: 'Pass',
+              End: true,
+            },
+            'EC2 Wait': {
+              Type: 'Wait',
+              Seconds: 5,
+              Next: 'DescribeInstanceStatus',
+            },
+          },
+        },
+      ],
+    },
+  };
+  new StateMachine(stack, 'Test', {
+    stateMachineName: 'A-nice-state-machine',
+    overrides: {
+      Branches: [{
+        StartAt: 'ResumeCluster',
+        States: {
+          ResumeCluster: {
+            Parameters: {
+              ClusterIdentifier: 'CLUSTER_NAME',
+            },
+          },
+          DescribeClusters: {
+            Parameters: {
+              ClusterIdentifier: 'CLUSTER_NAME',
+            },
+          },
+        },
+      }, {
+        StartAt: 'StartInstances',
+        States: {
+          StartInstances: {
+            Parameters: {
+              InstanceIds: ['INSTANCE_ID'],
+            },
+          },
+          DescribeInstanceStatus: {
+            Parameters: {
+              InstanceIds: ['INSTANCE_ID'],
+            },
+          },
+        },
+      }],
+    },
+    stateMachineType: StateMachineType.STANDARD,
+    definition: startingDefinition,
+  });
+  const assert = Template.fromStack(stack);
+  assert.hasResourceProperties('AWS::StepFunctions::StateMachine', {
+    DefinitionString: JSON.stringify({
+      States: {
+        Branches: [
+          {
+            StartAt: 'ResumeCluster',
+            States: {
+              'ResumeCluster': {
+                Type: 'Task',
+                Parameters: {
+                  ClusterIdentifier: 'CLUSTER_NAME',
+                },
+                Resource: 'arn:aws:states:::aws-sdk:redshift:resumeCluster',
+                Next: 'DescribeClusters',
+              },
+              'DescribeClusters': {
+                Type: 'Task',
+                Parameters: {
+                  ClusterIdentifier: 'CLUSTER_NAME',
+                },
+                Resource: 'arn:aws:states:::aws-sdk:redshift:describeClusters',
+                Next: 'Evaluate Cluster Status',
+              },
+              'Evaluate Cluster Status': {
+                Type: 'Choice',
+                Choices: [
+                  {
+                    Variable: '$.Clusters[0].ClusterStatus',
+                    StringEquals: 'available',
+                    Next: 'Redshift Pass',
+                  },
+                ],
+                Default: 'Redshift Wait',
+              },
+              'Redshift Pass': {
+                Type: 'Pass',
+                End: true,
+              },
+              'Redshift Wait': {
+                Type: 'Wait',
+                Seconds: 5,
+                Next: 'DescribeClusters',
+              },
+            },
+          },
+          {
+            StartAt: 'StartInstances',
+            States: {
+              'StartInstances': {
+                Type: 'Task',
+                Parameters: {
+                  InstanceIds: ['INSTANCE_ID'],
+                },
+                Resource: 'arn:aws:states:::aws-sdk:ec2:startInstances',
+                Next: 'DescribeInstanceStatus',
+              },
+              'DescribeInstanceStatus': {
+                Type: 'Task',
+                Next: 'Evaluate Instance Status',
+                Parameters: {
+                  InstanceIds: ['INSTANCE_ID'],
+                },
+                Resource: 'arn:aws:states:::aws-sdk:ec2:describeInstanceStatus',
+              },
+              'Evaluate Instance Status': {
+                Type: 'Choice',
+                Choices: [
+                  {
+                    And: [
+                      {
+                        Variable: '$.InstanceStatuses[0].InstanceState.Name',
+                        StringEquals: 'running',
+                      },
+                      {
+                        Variable: '$.InstanceStatuses[0].SystemStatus.Details[0].Status',
+                        StringEquals: 'passed',
+                      },
+                      {
+                        Variable: '$.InstanceStatuses[0].InstanceStatus.Details[0].Status',
+                        StringEquals: 'passed',
+                      },
+                    ],
+                    Next: 'EC2 Pass',
+                  },
+                ],
+                Default: 'EC2 Wait',
+              },
+              'EC2 Pass': {
+                Type: 'Pass',
+                End: true,
+              },
+              'EC2 Wait': {
+                Type: 'Wait',
+                Seconds: 5,
+                Next: 'DescribeInstanceStatus',
+              },
+            },
+          },
+        ],
+      },
+    }),
+  });
 });
