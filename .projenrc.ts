cd6b5da30cb04aec8585d3cd4b66d343657255e2
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2025-10-20 13:28:51 -0400

    feat: flexible construct usage.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,24 +1,34 @@
 import { CdkConstruct } from '@matthewbonig/cdk-construct-library';
 
 const lodash = 'lodash.merge';
-const projenDep = 'projen@^0.88.2';
+// const projenDep = 'projen@^0.88.2';
+const cdkVersion = '2.200.0'; // Required minimum CDK version of consumers of the library
+const cdkCliVersion = '2.1029.2';
+const projenVersion = '^0.95.4'; // Does not affect consumers of the library
+const minConstructsVersion = '10.0.5'; // Minimum version to support CDK v2
 const project = new CdkConstruct({
   description: 'A Step Function state machine construct focused on working well with the Workflow Studio',
-  cdkVersion: '2.85.0',
+  cdkVersion: cdkVersion,
+  projenVersion: projenVersion,
   name: 'state-machine',
-  constructsVersion: '10.3.0',
   deps: [
-    projenDep,
     lodash,
     'case',
     'js-yaml',
   ],
   peerDeps: [
-    projenDep,
-    'constructs@10.3.0',
+    'aws-cdk-lib', // recommend using version 189 or greater due to security updates
+    'projen',
   ],
   devDeps: [
-    projenDep,
+    `aws-cdk@${cdkCliVersion}`,
+    `aws-cdk-lib@${cdkVersion}`,
+    `constructs@^${minConstructsVersion}`,
+    'eslint@^9',
+    '@typescript-eslint/eslint-plugin@^8',
+    '@typescript-eslint/parser@^8',
+    'esbuild',
+    'projen',
     '@types/js-yaml',
     '@matthewbonig/cdk-construct-library@0.0.14',
   ],
@@ -29,6 +39,12 @@ const project = new CdkConstruct({
   disablePublishToNuGet: true,
 
 });
-project.github!.actions.set('actions/upload-artifact', 'actions/upload-artifact@v4.3.6');
+
+
+// Package Resolutions
+project.package.addField('resolutions', {
+  constructs: '^10.0.5',
+  projen: `${projenVersion}`,
+});
 
 project.synth();
