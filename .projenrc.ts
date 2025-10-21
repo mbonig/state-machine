1a20ba489cb72c0d72a2d356bbc76582f34b4b8c
Jayson Rawlins <JaysonJ.Rawlins@gmail.com>
2025-10-21 10:14:25 -0400

    build(ci): update GitHub Actions to v5 and v6
    - Upgraded several GitHub Actions to their latest versions, including `actions/checkout`, `actions/setup-node`, and `actions/upload-artifact`.
    - Updated `amannn/action-semantic-pull-request` to v6.
    - These updates ensure better compatibility and performance in CI workflows.

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,11 +1,11 @@
 import { CdkConstruct } from '@matthewbonig/cdk-construct-library';
 
 const lodash = 'lodash.merge';
-// const projenDep = 'projen@^0.88.2';
 const cdkVersion = '2.200.0'; // Required minimum CDK version of consumers of the library
 const cdkCliVersion = '2.1029.2';
-const projenVersion = '^0.95.4'; // Does not affect consumers of the library
-const minConstructsVersion = '10.0.5'; // Minimum version to support CDK v2
+const projenVersion = '^0.98.2'; // Does not affect consumers of the library
+const minConstructsVersion = '10.3.0'; // Minimum version to support CDK v2
+const cdkConstructProjectVersion = '0.0.17';
 const project = new CdkConstruct({
   description: 'A Step Function state machine construct focused on working well with the Workflow Studio',
   cdkVersion: cdkVersion,
@@ -18,33 +18,34 @@ const project = new CdkConstruct({
   ],
   peerDeps: [
     'aws-cdk-lib', // recommend using version 189 or greater due to security updates
+    `constructs@^${minConstructsVersion}`,
     'projen',
   ],
   devDeps: [
     `aws-cdk@${cdkCliVersion}`,
     `aws-cdk-lib@${cdkVersion}`,
-    `constructs@^${minConstructsVersion}`,
+    `constructs@${minConstructsVersion}`,
     'eslint@^9',
     '@typescript-eslint/eslint-plugin@^8',
     '@typescript-eslint/parser@^8',
     'esbuild',
     'projen',
     '@types/js-yaml',
-    '@matthewbonig/cdk-construct-library@0.0.14',
+    `@matthewbonig/cdk-construct-library@${cdkConstructProjectVersion}`,
   ],
+  constructsVersion: minConstructsVersion,
   bundledDeps: [lodash, 'case', 'js-yaml'],
   keywords: ['awscdk', 'cdk', 'AWS Step Functions'],
   disablePublishToGo: true,
   disablePublishToMaven: true,
   disablePublishToNuGet: true,
-
 });
-
 
 // Package Resolutions
 project.package.addField('resolutions', {
-  constructs: '^10.0.5',
+  constructs: `${minConstructsVersion}`,
   projen: `${projenVersion}`,
+  jsii: '^5.9.0',
 });
 
 project.synth();
